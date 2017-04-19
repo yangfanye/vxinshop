057d3907d388e5308e4dbe530c7200c627023b9b
yangfan <yf@cnmojie.com>
2017-04-19 15:22:53 +0800

    代码第一次上传

---
+++ Content/JS/mojie/mojie.js
@@ -0,0 +1,26 @@
+//获取url参数
+function getQueryString(name) { 
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+    var r = window.location.search.substr(1).match(reg); 
+    if (r != null) return unescape(r[2]); return null; 
+}
+//添加url参数
+function UrlUpdateParams(url, name, value) {
+    var r = url;
+    if (r != null && r != 'undefined' && r != "") {
+        value = encodeURIComponent(value);
+        var reg = new RegExp("(^|)" + name + "=([^&]*)(|$)");
+        var tmp = name + "=" + value;
+        if (url.match(reg) != null) {
+            r = url.replace(eval(reg), tmp);
+        }
+        else {
+            if (url.match("[\?]")) {
+                r = url + "&" + tmp;
+            } else {
+                r = url + "?" + tmp;
+            }
+        }
+    }
+    return r;
+}	
---
+++ data/category.js
@@ -0,0 +1,37 @@
+var category = [
+    {
+	value:'1',
+	text:'面膜',
+	children:[
+		{text:'唇膜',pic:'../../upload/mianmo/1.png'},
+		{text:'发膜',pic:'../../upload/mianmo/2.png'},
+		{text:'面膜贴',pic:'../../upload/mianmo/3.png'},
+		{text:'手膜',pic:'../../upload/mianmo/4.png'},
+		{text:'眼膜',pic:'../../upload/mianmo/5.png'},
+		{text:'足膜',pic:'../../upload/mianmo/6.png'}
+	]},
+	{
+	value:'2',
+	text:'护肤',
+	children:[
+		{text:'化妆水',pic:'../../upload/hufu/1.png'},
+		{text:'洁面',pic:'../../upload/hufu/2.png'},
+		{text:'精华',pic:'../../upload/hufu/3.png'},
+		{text:'面霜',pic:'../../upload/hufu/4.png'},
+		{text:'乳液',pic:'../../upload/hufu/5.png'},
+		{text:'眼霜',pic:'../../upload/hufu/6.png'}
+	]},
+	{
+	value:'3',
+	text:'百货',
+	children:[
+		{text:'杯子',pic:'../../upload/baihuo/1.png'},
+		{text:'家具清洁',pic:'../../upload/baihuo/2.png'},
+		{text:'口罩',pic:'../../upload/baihuo/3.png'},
+		{text:'暖宝宝',pic:'../../upload/baihuo/4.png'},
+		{text:'饰品',pic:'../../upload/baihuo/5.png'},
+		{text:'袜子',pic:'../../upload/baihuo/6.png'}
+	]},
+
+
+]
